2b85375c9c17b66e087d7a097246daeb35251b0b
Pinchai <>
2023-09-05 16:09:02 +0700

    respone

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello SS!'
+    return render_template('product_card.html')
 
 
 if __name__ == '__main__':
