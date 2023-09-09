e1653a7eca7ab923881648e8b28853d7a6dbccd0
Pinchai <>
2023-09-09 15:28:07 +0700

    jinja part 1

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
-
+import random
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('product_card.html')
+    products = []
+    for item in range(12):
+        products.append(
+            {
+                'id': 1,
+                'name': 'coca cola',
+                'old_price': random.randint(20, 500),
+                'discount': random.randint(1, 100),
+            },
+        )
 
+    return render_template('product_card.html', products=products)
 
 if __name__ == '__main__':
     app.run()
