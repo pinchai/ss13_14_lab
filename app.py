d17677524e836dd61c96a415c5c3b8371e510a9f
Pinchai <>
2023-09-09 16:52:14 +0700

    no message

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import random
+from essential_generators import DocumentGenerator
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    gen = DocumentGenerator()
     products = []
     for item in range(12):
         products.append(
@@ -13,10 +15,12 @@ def hello_world():
                 'name': 'coca cola',
                 'old_price': random.randint(20, 500),
                 'discount': random.randint(1, 100),
+                'description': gen.sentence()
             },
         )
 
     return render_template('product_card.html', products=products)
+
 
 if __name__ == '__main__':
     app.run()
